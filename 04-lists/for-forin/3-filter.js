b6dea187cc646b58b464602a63ac95fc538ca2c1
Willane Paiva <willanepaiva@hotmail.com>
2019-12-27 20:10:33 -0300

    add personalized filter

--- 04-lists/for-forin/3-filter.js
+++ 04-lists/for-forin/3-filter.js
@@ -1,9 +1,23 @@
 const { obterPessoas } = require("./service");
 
+Array.prototype.meuFilter = function(callback) {
+  const finalResult = [];
+  for (index in this) {
+    const item = this[index];
+    const result = callback(item, index, finalResult);
+    if (!result) continue;
+    finalResult.push(item);
+  }
+  return finalResult;
+};
+
 async function main() {
   try {
     const { results } = await obterPessoas("a");
-    const familiaLars = results.filter(
+    // const familiaLars = results.filter(
+    //   pessoa => pessoa.name.toLowerCase().indexOf("lars") !== -1
+    // );
+    const familiaLars = results.meuFilter(
       pessoa => pessoa.name.toLowerCase().indexOf("lars") !== -1
     );
     const names = familiaLars.map(pessoa => pessoa.name);
