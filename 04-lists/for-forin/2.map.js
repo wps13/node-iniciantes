46c040668e1d2e70bd596f5d4258085a1d3a15f7
Willane Paiva <willanepaiva@hotmail.com>
2019-12-27 18:37:35 -0300

    add personalized map

---
+++ 04-lists/for-forin/2.map.js
@@ -0,0 +1,29 @@
+const service = require("./service");
+
+Array.prototype.meuMap = function(callback) {
+  const novoArrayMapeado = [];
+  for (let i = 0; i <= this.length - 1; i++) {
+    const resultado = callback(this[i]);
+    novoArrayMapeado.push(resultado);
+  }
+  return novoArrayMapeado;
+};
+
+async function main() {
+  try {
+    const result = await service.obterPessoas("a");
+
+    // const names = [];
+
+    // result.results.forEach(element => {
+    //   names.push(element.name);
+    // });
+    // const names = result.results.map(pessoa => pessoa.name);
+    const names = result.results.meuMap(pessoa => pessoa.name);
+    console.log(`names: ${names}`);
+  } catch (error) {
+    console.error("erro", error);
+  }
+}
+
+main();
