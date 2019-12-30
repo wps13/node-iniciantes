638f1f30d55884bd6c77398899978688074deec1
Willane Paiva <willanepaiva@hotmail.com>
2019-12-30 18:57:42 -0300

    add personalized reduce

--- 04-lists/for-forin/4.reduce.js
+++ 04-lists/for-forin/4.reduce.js
@@ -1,5 +1,14 @@
 const { obterPessoas } = require("./service");
 
+Array.prototype.meuReduce = function(callback, initialValue) {
+  let acumulador = initialValue || this[0];
+
+  for (item of this) {
+    acumulador = callback(acumulador, item);
+  }
+  return acumulador;
+};
+
 async function main() {
   try {
     const { results } = await obterPessoas("a");
@@ -7,8 +16,8 @@ async function main() {
     const pesos = results.map(pessoa =>
       pessoa.mass !== "unknown" ? parseInt(pessoa.mass) : 0
     );
-    const total = pesos.reduce((anterior, proximo) => anterior + proximo);
-
+    // const total = pesos.reduce((anterior, proximo) => anterior + proximo);
+    const total = pesos.meuReduce((anterior, proximo) => anterior + proximo);
     console.log("soma de pesos", total);
   } catch (error) {
     console.error("error", error);
