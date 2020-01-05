0003a81741129d63788d5a37f7c4770004426304
Willane Paiva <willanepaiva@hotmail.com>
2020-01-04 21:05:58 -0300

    add comman line interface

---
+++ 06-cli/index.js
@@ -0,0 +1,52 @@
+const Commander = require("commander");
+const Database = require("./database");
+const Heroi = require("./heroi");
+
+async function main() {
+  Commander.version("v1")
+    .option("-n, --nome [value]", "Nome do heroi")
+    .option("-p, --poder [value]", "Poder do heroi")
+    .option("-i, --id [value]", "Id do heroi")
+    .option("-c, --cadastrar", "Cadastrar um heroi")
+    .option("-l, --listar", "Listar os herois cadastrados")
+    .option("-r, --remover", "remove um heroi")
+    .option("-a, --atualizar", "atualiza as informacoes de um heroi")
+    .parse(process.argv);
+
+  const heroi = new Heroi(Commander);
+
+  try {
+    if (Commander.cadastrar) {
+      const resultado = await Database.cadastrar(heroi);
+      if (!resultado) {
+        console.error("heroi nao foi cadastrado");
+        return;
+      }
+      console.log("heroi cadastrado");
+    }
+    if (Commander.listar) {
+      const resultado = await Database.listar();
+      console.log("herois cadastrados", resultado);
+    }
+    if (Commander.remover) {
+      const resultado = await Database.remover(heroi.id);
+      if (!resultado) {
+        console.error("erro ao remover o heroi");
+        return;
+      }
+      console.log("heroi removido com sucesso");
+    }
+    if (Commander.atualizar) {
+      const resultado = await Database.atualizar(heroi.id, heroi);
+      if (!resultado) {
+        console.error("heroi nao foi atualizado");
+        return;
+      }
+      console.log("heroi atualizado com sucesso");
+    }
+  } catch (error) {
+    console.error("error", error);
+  }
+}
+
+main();
